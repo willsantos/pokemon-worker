36e875a8a5563aa1c8d6487c459d9a6a3bebdb96
Wilson Santos <contato@wilsonsantos.net>
2024-04-05 21:20:34 -0300

    initial commit

---
+++ WS.Pokedex.Worker/Program.cs
@@ -0,0 +1,17 @@
+namespace WS.Pokedex.Worker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
